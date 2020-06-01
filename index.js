533c11aa749367814255f911dff06af8f1e4987f
saharei <saharai.co@gmail.com>
2020-06-02 06:24:46 +0900

    修正：dbからstep4と5の値を呼び出す　一部修正

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get("/", function (req, res, next) {
       console.log(err);
     } else {
       client.query(
-        "SELECT a.step4, b.step5 FROM step4s a, step5s b WHERE roomid = $1",
+        "SELECT a.step4, b.step5 FROM step4s a, step5s b WHERE a.roomid = $1, AND b.roomid $1",
         ["5ecdb3259323a"],
         function (error, results) {
           if (error) {
